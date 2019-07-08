d1fa183916ec9544567b75e0336d2209173e0080
Jemila <jemila.abulhawa@gmail.com>
2019-07-08 11:16:33 +0300

    es6ify step9 solution / modularise code

--- README.md
+++
@@ -1,2 +0,0 @@
-# workshop-cms-complete
-The completed version of the CMS workshop for February 2016
---
+++ src/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const router = require('./router')
+
+const port = 4000;
+
+const server = http.createServer(router);
+
+server.listen(port);
+
+console.log(`server up and running on localhost:${port}`);
